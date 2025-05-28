be765df4938d1dbea49b624f0798fa88dae0d7b4
aidanlipp <alippencott29@gmail.com>
2025-05-28 11:04:29 -0500

    Create App.js

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import GameChangerAnalyzer from './components/GameChangerAnalyzer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <GameChangerAnalyzer />
+    </div>
+  );
+}
+
+export default App;
